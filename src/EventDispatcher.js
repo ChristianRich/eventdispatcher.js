957144196aaead3e0825603e4e47016ce87a2e19
Christian Schlosrich <christian.schlosrich@make.com.au>
2013-03-11 17:01:41 +1100

    Removing linebreaks

--- src/EventDispatcher.js
+++ src/EventDispatcher.js
@@ -7,19 +7,13 @@ var EventDispatcher = function () {
 	var listeners = {};
 
 	this.addEventListener = function ( type, listener ) {
-
 		if ( listeners[ type ] === undefined ) {
-
 			listeners[ type ] = [];
-
 		}
 
 		if ( listeners[ type ].indexOf( listener ) === - 1 ) {
-
 			listeners[ type ].push( listener );
-
 		}
-
 	};
 	
 	this.hasEventListener = function( type ){
@@ -31,37 +25,27 @@ var EventDispatcher = function () {
 	};
 	
 	this.removeEventListener = function ( type, listener ) {
-		
 		if(!this.hasEventListener( type )){
-			return false;
+			return;
 		}
 		
 		var index = listeners[ type ].indexOf( listener );
 
 		if ( index !== - 1 ) {
-
 			listeners[ type ].splice( index, 1 );
-
 		}
-
 	};
 
 	this.dispatchEvent = function ( event ) {
-
 		var listenerArray = listeners[ event.type ];
 
 		if ( listenerArray !== undefined ) {
-			
+
 			event.target = this;
 
 			for ( var i = 0, l = listenerArray.length; i < l; i ++ ) {
-
 				listenerArray[ i ].call( this, event );
-
 			}
-
 		}
-
 	};
-
 };
